c00da102576463a1a40b4e02b1a72d3fd57f9a5f
Felix Bünemann <buenemann@louis.info>
2016-08-12 22:20:53 +0200

    fix(model): Specify a default ssl cipher list
    This specifies a default cipher list instead of relying on the defaults
    used by openssl, which are not tailored to https and cause issues with
    HTTP/2 in Firefox due to poor cipher order.
    
    The defaults are chosen to be secure, fast and compatible with current
    clients, but exclude IE6 on XP and Java6 from connecting.
    
    The list was built using current recommendation from the Mozilla Wiki
    at https://wiki.mozilla.org/Security/Server_Side_TLS for intermediate
    compatibility.
    
    Ciphers are preferred in this order:
    
    - Prefer 128-Bit over 256-Bit encryptions (lower coputational overhead)
    - Prefer GCM over EDH over RSA auth (GCM & EDH support Forward Secrecy)
    - Fallback to 112-Bit 3DES (mainly for IE 8 compatibility)

--- model/model.go
+++ model/model.go
@@ -166,6 +166,14 @@ func newSSLConfig() *SSLConfig {
 	return &SSLConfig{
 		Enforce:           false,
 		Protocols:         "TLSv1 TLSv1.1 TLSv1.2",
+		// Default cipher suite:
+		//  - Prefer 128-Bit over 256-Bit encryptions (lower overhead)
+		//  - Prefer GCM over EDH over RSA auth (for Forward Secrecy)
+		//  - Fallback to 112-Bit 3DES (mainly for IE 8 compatibility)
+		// Compatible: Firefox 1, Chrome 1, IE 7, Opera 5, Safari 1, Windows XP IE8, Android 2.3, Java 7
+		// Incompatible: Windows XP IE6, Java 6
+		// Source: https://wiki.mozilla.org/Security/Server_Side_TLS (intermediate compatiblity)
+		Ciphers:           "ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA:ECDHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA256:DHE-RSA-AES256-SHA:ECDHE-ECDSA-DES-CBC3-SHA:ECDHE-RSA-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:DES-CBC3-SHA:!DSS",
 		SessionTimeout:    "10m",
 		UseSessionTickets: true,
 		BufferSize:        "4k",
